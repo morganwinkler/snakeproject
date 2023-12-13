9fd40647142b6ea0c4ef2baaf0594f2396463653
Morgan Winkler <103147883+morganwinkler@users.noreply.github.com>
2023-12-13 13:24:28 -0500

    test

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+app = Flask(__name__)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    return jsonify({'id': book.id, 'title': book.title, 'author': book.author})
+
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.get_json()
+    new_book = Book(title=data['title'], author=data['author'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({'id': new_book.id, 'title': new_book.title, 'author': new_book.author}), 201
